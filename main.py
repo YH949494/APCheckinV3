f0302ca27e57ce7ab2c3e4dced3ce6a5fe9ee8ad
YH949494 <teohyaohui@gmail.com>
2025-07-18 10:02:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
-TELEGRAM_TOKEN = 'YOUR_BOT_TOKEN'
+TELEGRAM_TOKEN = '7620072160:AAEbty9W7vqZ5sgNAY8I0ACWYCIiNVrNZDs'
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message
